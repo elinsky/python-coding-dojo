831ee3761cd48af870ab1ffd666ba42539a1a5a1
Brian Elinsky <brian@brianelinsky.com>
2025-11-13 22:11:57 -0600

    Revert parity.py to empty template
    Keep problem files empty after solving for clean practice.

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,17 +2,8 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # variable to track parity. either 0 or 1.
-    par = 0
-    # could iterate over all the bits
-    # faster would be iterate over 1s
-    # forget how to do that tho
-    while x:
-        # isolate last bit
-        last_bit = x & 1
-        par = par ^ last_bit
-        x = x >> 1
-    return par
+    # TODO - you fill in here.
+    return 0
 
 
 if __name__ == '__main__':
